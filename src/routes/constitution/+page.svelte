3f57b22c2c8dc98d96569cf312e6e65405e51d28
crawlbaby96 <christophercrawlings@gmail.com>
2024-09-14 13:07:20 -0400

    updated constitution page to embed google docs

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -1,5 +1,92 @@
 <script>
     import { dues } from '$lib/utils/helper';
+
+    let one, oneOne, oneOneOne, oneTwo, oneTwoOne, oneTwoTwo, oneTwoThree, oneTwoFour, oneTwoFive, oneThree;
+    let two, twoOne, twoTwo, twoThree;
+    let three, threeOne;
+    let four, fourOne, fourTwo;
+    let five, fiveOne;
+    let six, sixOne, sixTwo, sixThree;
+    let seven, sevenOne, sevenTwo, sevenThree;
+
+    const goToSection = (section) => {
+        const top = section.getBoundingClientRect().top + window.pageYOffset;
+        window.scrollTo({ left: 0, top, behavior: 'smooth' });
+    }
+</script>
+
+<style>
+    /* Ensure the entire page is filled with the iframe and it's centered */
+    html, body {
+        height: 100%;
+        margin: 0;
+    }
+
+    
+
+    .constitution {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: right;
+        width: 100vw;
+        height: 90vh;
+        margin: 0;
+        padding: 0;
+    }
+
+    .google-doc-embed {
+        width: 100%;
+        height: 90%;
+    }
+
+    .google-doc-embed iframe {
+        width: 100%;
+        height: 100%;
+        border: 2px solid green;
+    }
+     
+    /* Optional: Add CSS to ensure the embedded doc takes up full space */
+    .google-doc-embed iframe {
+        zoom: 1.3; /* Adjust the zoom level to ensure it looks full-screen */
+        transform: scale(1.0);
+        transform-origin: top right;
+    }
+     .title {
+        font-size: 2em;
+        text-align: center;
+        margin-bottom: 10px;
+        font-family: sans-serif;
+        color: #00316b;
+    }
+</style>
+
+<div class="constitution">
+    <h1 class="title">G-Vegas Gridiron</h1>
+
+    <div class="google-doc-embed">
+        <iframe src="https://docs.google.com/document/d/1QvbwNAI5z6j0ghQEzkZtkdGeN9z0un4L1CHH6TR0hek/pub?embedded=true"></iframe>
+    
+    
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- <script>
+    import { dues } from '$lib/utils/helper';
     let one, oneOne, oneOneOne, oneTwo, oneTwoOne, oneTwoTwo, oneTwoThree, oneTwoFour, oneTwoFive, oneThree;
     let two, twoOne, twoTwo, twoThree;
     let three, threeOne;
@@ -300,4 +387,4 @@
     <h3 bind:this={sevenThree}>7.3 Raising Dues</h3>
     <p>A ⅔ majority vote is required in order to raise league dues. In the event that a manager is no longer financially comfortable with the buyin, finding a co-manager to split the cost is reccomended.</p>
 
-</div>
+</div> -->
