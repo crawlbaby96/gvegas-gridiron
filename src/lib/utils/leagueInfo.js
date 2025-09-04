6d5843a7976bb6444ce1f2db1a816cbcee2c9ff6
crawlbaby96 <christophercrawlings@gmail.com>
2025-09-04 15:19:26 -0400

    Resources: add YouTube iframe above Spotify embed

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -17,6 +17,7 @@ export const homepageText = `
 
 // Podcast embeds for Resources page
 export const podcastEmbeds = [
+  `<iframe width="560" height="315" src="https://www.youtube.com/embed/PjNEVrCrJ4s?si=XKn65xQRCtxR7Lw8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
   `<iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/episode/27nHorVZT9yFET2cF1jW8F?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`
 ];
 
